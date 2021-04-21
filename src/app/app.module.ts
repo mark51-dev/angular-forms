ecd8300db799b4c40f693900eb7427c093d53e1b
sb0na <mouzy724@gmail.com>
2021-04-21 18:32:43 +0300

    Initial commit (empty project)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
